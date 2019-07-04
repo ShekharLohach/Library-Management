d16c4d859ca87342777e4e03c5ddd0bfb207b22b
Shekhar Lohach <shekhar.lohach9p6@gmail.com>
2019-07-04 22:39:57 +0530

    updated file
    File with more function such as return book or issue book

--- Admin.py
+++ Admin.py
@@ -18,6 +18,39 @@ with open("books_list.txt",'a+') as f:
                 print(i,end="")
 
 
+    def book_issue():
+        with open("books_list.txt", 'r') as f:
+            text = f.read().strip()
+            book_issue = int(input("enter the books you want to issue"))
+            for i in range(book_issue):
+                book_id=input("enter the id of book")
+                with open("books_list.txt",'r') as rf:
+
+                    if(book_id in text):
+                        with open("issued_books.txt","w") as wf:
+                            wf.write("\n")
+                            wf.write(book_id),wf.write(" ")
+                            wf.write('%s' %datetime.now())
+                            wf.write("\n")
+                            print("Book has been isued")
+                    else:
+                        print("Book dosen't exist")
+
+# book_issue()
+with open("issued_books.txt",'r') as rf:
+    def return_book():
+        book_return = int(input("enter the books you want to return"))
+        for i in range(book_return):
+            book_id = input("enter the id of book")
+            with open("issued_books.txt", 'r') as rf:
+                text = rf.read().strip().split()
+            with open("issued_books.txt", "w") as wf:
+                for i in text:
+                    if(i!=book_id):
+                        wf.write("\n"), wf.write(i)
+                        wf.write("\n")
+
+# return_book()
 
 
 
