6582a4fb748f3a5a35e56ae61c62e40e2c109ebc
Shekhar Lohach <shekhar.lohach9p6@gmail.com>
2019-07-05 20:07:57 +0530

    Final_change_with_all_functions

--- Admin.py
+++ Admin.py
@@ -18,6 +18,13 @@ with open("books_list.txt",'a+') as f:
                 print(i,end="")
 
 
+
+
+
+
+
+
+
     def book_issue():
         with open("books_list.txt", 'r') as f:
             text = f.read().strip()
@@ -37,6 +44,7 @@ with open("books_list.txt",'a+') as f:
                         print("Book dosen't exist")
 
 # book_issue()
+
 with open("issued_books.txt",'r') as rf:
     def return_book():
         book_return = int(input("enter the books you want to return"))
@@ -51,6 +59,62 @@ with open("issued_books.txt",'r') as rf:
                         wf.write("\n")
 
 # return_book()
+
+def search():
+    book_id = input("enter the id of book")
+    with open("books_list.txt", 'r') as rf:
+        text=rf.read().split()
+
+        with open("issued_books.txt", 'r') as rf:
+            issue=rf.read().split()
+            if( book_id in text):
+                if(book_id in issue):
+                    print("Book is already issued")
+                else:
+                    if(book_id in text):
+                        print("Book is there")
+                    else:
+                        def prints():
+                            print("Book dosen't exist")
+
+def all_books():
+    with open("books_list.txt", 'r') as rf:
+        for i in rf:
+            print(i)
+
+def exit():
+    exit="f"
+
+########################################### Main.py##################################
+
+def main():
+    exit="t"
+    while(exit=="t"):
+        print(""" ======LIBRARY MENU=======
+                          1. Display all available books
+                          2. Request a book
+                          3. Return a book
+                          4. Search a book
+                          5. Add a book
+                          6. Exit
+                          """)
+
+        choose=int(input("enter your choice"))
+        if(choose==1):
+            all_books()
+        elif(choose==2):
+            book_issue()
+        elif(choose==3):
+            return_book()
+        elif(choose==4):
+            search()
+        elif(choose==5):
+            add_books()
+        else:
+            sys.exit()
+main()
+
+
 
 
 
